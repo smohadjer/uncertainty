bcdfd258f5827552cf9b8851919431b10c211de4
Saeid Mohadjer <saeid@fastmail.fm>
2025-01-20 21:02:54 +0100

    Initialize develop

---
+++ app/resources/js/main.js
@@ -0,0 +1,40 @@
+import Navigation from './modules/nav.js';
+
+//Helper classes to HTML for styling of nojs version
+const html = document.querySelector('html');
+html.classList.remove('no-js');
+html.classList.add('js');
+
+//taken from http://youmightnotneedjquery.com/
+function ready(fn) {
+	'use strict';
+
+	if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
+		fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
+}
+
+ready(function() {
+	'use strict';
+
+	console.log('DOM is ready!');
+
+	//initialize navigation
+	const nav = new Navigation({
+		element: document.querySelector('header > nav')
+	});
+
+	//hamburger button
+	const hamburger = document.querySelector('button.hamburger');
+	if (hamburger) {
+		hamburger.addEventListener('click', function() {
+			hamburger.classList.toggle('is-active');
+
+			if (nav.el) {
+				nav.el.classList.toggle('is-visible');
+			}
+		});
+	}
+});
